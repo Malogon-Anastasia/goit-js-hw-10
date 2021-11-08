e2ba25ab9318d2e0f0bcd2fc1ef26630945a67bc
Malogon Anastasua <malogonanastasia@gmail.com>
2021-11-08 11:16:56 +0200

    update js files

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,8 +1,8 @@
 export default fetchCountries;
 
 const BASE_URL = 'https://restcountries.com/#api-endpoints-v3-name';
-function fetchCountries(searchQuery) {
-    return fetch(`${BASE_URL}${searchQuery}`)
+function fetchCountries(name) {
+    return fetch(`${BASE_URL}${name}`)
         .then(response => {
             if (!response.ok) {
                 throw Error();
